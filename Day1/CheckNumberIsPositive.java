1568ddbde8af5841c78ea49ab24bd1c10d67108d
ManiSrinivas7363 <manisrinivas7363@gmail.com>
2025-07-22 16:01:51 +0530

    Add files via upload
    Day1 Assignment

---
+++ Day1/CheckNumberIsPositive.java
@@ -0,0 +1,17 @@
+package TestLeafAssignments.Week1;
+
+public class CheckNumberIsPositive {
+
+	public static void main(String[] args) {
+		int version=10;
+		
+		if (version < 0) {
+		System.out.println("Number Negative");
+		}
+		else if (version > 0) {
+			System.out.println("Number positive");
+		}
+
+	}
+
+}
